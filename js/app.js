0b57e52376e1888cd3c7a3fafc595299e16e8cc9
avnit-anand <avnitanand1806@gmail.com>
2023-04-14 14:41:25 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position;
+    if (navbar.classList.contains("fixed")) {
+      position = element.offsetTop - 62;
+    } else {
+      position = element.offsetTop - 124;
+    }
+    if (window.innerWidth < 992) {
+      if (navbar.classList.contains("fixed")) {
+        position = element.offsetTop - 62;
+      } else {
+        position = element.offsetTop - 332 - 62;
+      }
+    }
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
